1707192cf2da8f195ab45d275c33224c3efb15b7
oxfordblucher <hiuchanhk@gmail.com>
2020-12-08 18:48:32 -0500

    fixed up questions array

---
+++ app/questions.js
@@ -0,0 +1,293 @@
+module.exports = {
+    questions:[
+        {
+            question: "You are sleeping next to your partner and they are taking up two thirds of the space, you...",
+            options: {
+                optionA: {
+                    name:"closer the better! Who needs space?",
+                    keys: "all"
+                },
+                optionB: {
+                    name: "grumpily push them over, it's so inconsiderate of your needs!",
+                    keys: [4,5,7,10,11,12,13,16,18,20,21,22,24,25]
+                },
+                optionC: {
+                    name: "It's not ideal but you make it work!",
+                    keys: [1,3,4,5,7,8,9,10,11,12,13,14,15,16,18,20,21,22,24,25]
+                }
+            }
+        },
+        {
+            question: "the check comes to the table, do you...",
+            options: {
+                optionA: {
+                    name: "jump at the offer to pay the bill?", 
+                    keys: "all"
+                },
+                optionB: {
+                    name: "pull up your calculator app, let's split it!",
+                    keys: [1,3,9,10,13,14,15,18,20,21,22,25]
+                },
+                optionC: {
+                    name: "Let everyone know that you only got a salad and don't want to split",
+                    keys: [7,12,24]
+                }
+            }
+        },
+        {
+            question: "You are sleeping next to your partner and they are taking up two thirds of the space, you...",
+                options: {
+                    optionA: {
+                        name:"closer the better! Who needs space?",
+                        keys: "all"
+                    },
+                    optionB: {
+                        name: "grumpily push them over, it's so inconsiderate of your needs!",
+                        keys: [4,5,7,10,11,12,13,16,18,20,21,22,24,25]
+                    },
+                    optionC: {
+                        name: "It's not ideal but you make it work!",
+                        keys: [1,3,4,5,7,8,9,10,11,12,13,14,15,16,18,20,21,22,24,25]
+                    }
+                }
+        },
+        {
+            question: "the check comes to the table, do you...",
+            options: {
+                optionA: {
+                    name: "jump at the offer to pay the bill?", 
+                    keys: "all"
+                },
+                optionB: {
+                    name: "pull up your calculator app, let's split it!",
+                    keys: [1,3,9,10,13,14,15,18,20,21,22,25]
+                },
+                optionC: {
+                    name: "Let everyone know that you only got a salad and don't want to split",
+                    keys: [7,12,24]
+                }
+            }
+        },
+        {
+            question: "It is 10am on Saturday morning, your day begins with...",
+            options: {
+                optionA: {
+                    name: "It began at 5am, I am already halfway through my errands!", 
+                    keys: [13,15,21]
+                },
+                optionB: {
+                    name: "smash that snooze button!",
+                    keys: [4,5,11,12,16,20,22,23]
+                },
+                optionC: {
+                    name: "I've had a cup of coffee and am reading the paper",
+                    keys: [1,2,3,6,7,8,9,10,14,17,18,19,24,25]
+                },
+            }
+        },
+        {
+            question: "Your friend spilled red wine on your carpet, you...",
+            options: {
+                optionA: {
+                    name:  "It's fine, the rug is old and I'll just hide that corner under the couch!",
+                    keys: "all"
+                },
+                optionB: {
+                    name: "I've already called the carpet cleaner and sent my friend a copy of the bill",
+                    keys: [4,6,7,9,11,12,14,18,19,20,21,22,24]
+                },
+                optionC: {
+                    name: "I think I've got a stain stick somewhere, or some club soda?",
+                    keys: [1,4,5,6,7,8,9,10,11,12,14,17,18,19,20,21,22,24]
+                }
+            }
+        },
+        {
+            question: "You meet someone for the first time at a party and your friend thinks they were unfriendly...",
+            options: {
+                optionA: {
+                    name: "Totally, such bad vibes",
+                    keys: [1,3,4,5,6,10,12,13,14,16,17,19,20,22,23,24,25]
+                }, 
+                optionB: {
+                    name: "Maybe? We did just meet, some people don't give great first impressions",
+                    keys: [2,7,11,18,21,]
+                },
+                optionC: {
+                    name: "I am sure they were just shy! I'd love to talk to them more, I am sure there is a reason they got invited!",
+                    keys: "all"
+                }
+            }
+        },
+        {
+            question: "You're headed to the airport, where is your passport?",
+            options: {
+                optionA: {
+                    name: "somewhere in my bag, oh whoops it's unzipped!",
+                    keys: "all"
+                }, 
+                optionB: {
+                    name: "in my pocket, easily accessible for going through security",
+                    keys: [1,2,3,6,8,9,14,15,17,19,23]
+                },
+                optionC: {
+                    name:"in a hidden pouch attached to my body, there are pick-pockets you know!",
+                    keys: [2,8,9,15,19]
+                }
+            }
+        },
+        {
+            question: "You find out your teacher has a favorite student, you...",
+            options: {
+                optionA: {
+                    name: "think that is unfair, all students should be treated equally",
+                    keys: [2,6,8,10,11,13,16,18,19,20,21,22,23,24]
+                }, 
+                optionB: {
+                    name: "I guess that is an inevitability, they are human after all, and not all students produce the same work",
+                    keys: "all"
+                },
+                optionC: {
+                    name: "hope it is you, it'll be SO dissapointing if it isn't you",
+                    keys: [2,6,8,10,11,13,16,18,19,20,21,22,23,24]
+                }
+            }
+        },
+        {
+            question: "You have two friends that are in a fight, you: a. make plans with them separately, you hope they work it out but it isn't your job to manage their relationship",
+            options: {
+                optionA: {
+                    name: "make plans with them separately, you hope they work it out but it isn't your job to manage their relationship",
+                    keys: [2,8,9,11,15,19,25]
+                },
+                optionB: {
+                    name: "Invite them over to mediate, can't we all get along?",
+                    keys: "all"
+                }, 
+                optionC: {
+                    name: "Invite the friend you have a stronger relationship with over. You hate drama",
+                    keys: [8,15,19]
+                }
+            }
+        },
+        {
+            question: "Your car needs an expensive fix to pass inspection, you...",
+            options: {
+                optionA: {
+                    name: "are glad you have an emergency fund just for this type of occassion!",
+                    keys: "all"
+                }, 
+                optionB: {
+                    name: "Weigh your options, the car does have a lot of miles? Should you just sell it?",
+                    keys: [1,3,4,5,6,7,12,13,14,16,19,20,23,25]
+                },
+                optionC: {
+                    name: "I am a student, I don't have that kind of money! Back to the bus I suppose...",
+                    keys: [8,9,10,15,18,22,24]
+                }
+            }
+        },
+        {
+            question: "You show up to your cousin's wedding and your friend is wearing the exact same jumpsuit, you...",
+            options: {
+                optionA: {
+                    name: "love it! Let's get a picture together!",
+                    keys: "all"
+                },
+                optionB: {
+                    name: "Turn right around, you packed another outfit just in case",
+                    keys: [4,5,8,9,10,16,18,19,20,22,23,25]
+                }, 
+                optionC: {
+                    name: "Who cares? You love the outfit and it looks great on you, where's the dance floor??",
+                    keys: "all"
+                }
+            }
+        },
+        {
+            question: "You're looking for a house to buy and your realtor suggests a home close to train tracks, you think...",
+            options: {
+                optionA: {
+                    name: "Deal breaker, I'm a light sleeper as it is, plus I work from home!",
+                    keys: [4,5,9,13,16,20,22,23]
+                },
+                optionB: {
+                    name: "It is a beautiful home, I am sure I'll adapt to the sounds over time, let's see it!",
+                    keys: "all"
+                }, 
+                optionC: {
+                    name: "Just how close is it...?",
+                    keys: [1,6,10,11,12,14,15,17,25]
+                }
+            }
+        },
+        {
+            question: "Your friend joined a new gym and is trying to get you to join with her, you say...",
+            options: {
+                optionA: {
+                    name: "Definitely! It'll be great to workout with a friend",
+                    keys: [1,2,3,8,10,14,19,21]
+                },
+                optionB: {
+                    name: "No thank you. I don't need to pay for something I won't use",
+                    keys: [4,5,7,11,12,16,17,18,20,22,23,24]
+                }, 
+                optionC: {
+                    name: "Maybe. You already belong to a gym and frequent several studios to diverisfy my weekly workout regimine, but what do they offer?",
+                    keys: [6,9,13,15,25]
+                }
+            }
+        },
+        {
+            question: "Your idea of grocery shopping with your partner looks like:",
+            options: {
+                optionA: {
+                    name: "Divide and conquer, let's make this errand go by quickly!",
+                    keys: [7,8,13,15,19,21]
+                },
+                optionB: {
+                    name: "Slowly go through the aisles and look for inspiration, hey look at how good these apples look!",
+                    keys: [1,2,3,4,5,6,9,10,11,12,14,16,17,18,20,22,23,24,25]
+                }, 
+                optionC: {
+                    name: "",
+                    keys: []
+                }
+            }
+        },
+        {
+            question: "Your nephew is at an inquisitive age and has locked his attention and questions in on you. Your response:",
+            options: {
+                optionA: {
+                    name: "Hey buddy, where are your parents? You're not interested in entertaining him",
+                    keys: [4,5,7]
+                },
+                optionB: {
+                    name: "Thoughtfully answering all of his questions and marvelling to yourself about how interesting his thought process is at that age",
+                    keys: [1,2,3,6,8,9,13,19,25]
+                }, 
+                optionC: {
+                    name: "Hang out for a bit with him and have fun but eventually distract him with a new activity and make your escape",
+                    keys: [10,11,12,14,15,16,17,18,20,21,22,23,24]
+                }
+            }
+        },
+        {
+            question: "You got an amazing job offer in another city, which means leaving your best friend and wonderful apartment behind, you...",
+            options: {
+                optionA: {
+                    name: "Accept the offer, you'll miss your friend and the life you built but there are new relationships and life experiencing awaiting you, that's life",
+                    keys: "all"
+                },
+                optionB: {
+                    name: "Look for opportunities in your city, great friendships like this aren't worth leaving behind!",
+                    keys: [7,11,12,16,18,20,21,22,24]
+                }, 
+                optionC: {
+                    name: "Weigh your options, you have to look at the bigger picture here!",
+                    keys: [1,2,3,4,5,6,8,9,10,13,14,15,17,19,23,25]
+                }
+            }
+        }
+    ]
+};
--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,18 +1,28 @@
 const db = require("../models/index");
 const path = require("path");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-const {questions} = require("../public/assets/js/pups")
+const {questions} = require("../questions")
 
 module.exports = function(app) {
     
 
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../models/User.js"));
+      /*   v
+      ar questionObj={
+            questions: questions
+        }
+        console.log(questionObj); */
+        res.render("index");
     });
 
     app.get("/questions/:id", function(req, res) {
         console.log(questions[0]);
         res.render("index", questions[0]);
+    });
+
+    app.get("/getquestion/:number", function(req, res) {
+        
+        res.render("index", questions[req.params.number]);
     });
 
     /*app.get("/signIn", function(req, res) {
