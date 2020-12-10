4e126c67b94fbccb13ccaafdb61704fe56b51d0a
oxfordblucher <hiuchanhk@gmail.com>
2020-12-10 11:29:50 -0500

    json object passthrough

--- app/public/assets/js/logic.js
+++ app/public/assets/js/logic.js
@@ -1,3 +1,5 @@
+const { JSON } = require("sequelize/types");
+
 const quiz = $("div#quiz");
 
 function startQuiz() {
@@ -7,5 +9,27 @@ function startQuiz() {
 
 
 function answerQ(data) {
-    console.log(data);
+    const qNum = parseInt(data.qID);
+    const answrKeys = data.keys;
+    saveLocally(answrKeys);
 };
+
+/* function SaveDataToLocalStorage(data)
+{
+    var a = [];
+    // Parse the serialized data back into an aray of objects
+    a = JSON.parse(localStorage.getItem('session')) || [];
+    // Push the new data (whether it be an object or anything else) onto the array
+    a.push(data);
+    // Alert the array value
+    alert(a);  // Should be something like [Object array]
+    // Re-serialize the array back into a string and store it in localStorage
+    localStorage.setItem('session', JSON.stringify(a));
+} */
+
+function saveLocally(data) {
+    let keyArray = [];
+    /* keyArray = JSON.parse(localStorage.getItem('pupQuiz')) || []; */
+    keyArray.push(data);
+    localStorage.setItem('pupQuiz', JSON.stringify(keyArray));
+}
