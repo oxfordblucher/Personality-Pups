188565f7bc3566a8aedba57d15f7237aebb4b881
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-12-11 10:49:49 -0500

    added more styling,edited questions for grammar and capitalization, punctuation. deleted login.handlebars, and fixed navbar

--- app/questions.js
+++ app/questions.js
@@ -1,35 +1,35 @@
 module.exports = {
     questions:[
         {
-            question: "You are sleeping next to your partner and they are taking up two thirds of the space, you...",
+            question: "You are sleeping next to your partner and they are taking up two thirds of the space...",
             options: {
                 optionA: {
-                    name:"closer the better! Who needs space?",
+                    name:"Closer the better! Who needs space?",
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 },
                 optionB: {
-                    name: "grumpily push them over, it's so inconsiderate of your needs!",
+                    name: "Grumpily push them over, it's so inconsiderate of your needs!",
                     keys: [4,5,7,10,11,12,13,16,18,20,21,22,24,25]
                 },
                 optionC: {
-                    name: "It's not ideal but you make it work!",
+                    name: "It's not ideal but you can make it work!",
                     keys: [1,3,4,5,7,8,9,10,11,12,13,14,15,16,18,20,21,22,24,25]
                 }
             }
         },
         {
-            question: "the check comes to the table, do you...",
+            question: "The check comes to the table, you...",
             options: {
                 optionA: {
-                    name: "jump at the offer to pay the bill?", 
+                    name: "Jump at the offer to pay the bill?", 
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 },
                 optionB: {
-                    name: "pull up your calculator app, let's split it!",
+                    name: "Pull up your calculator app, let's split it!",
                     keys: [1,3,9,10,13,14,15,18,20,21,22,25]
                 },
                 optionC: {
-                    name: "Let everyone know that you only got a salad and don't want to split",
+                    name: "Let everyone know that you only got a salad and don't want to split.",
                     keys: [7,12,24]
                 }
             }
@@ -42,17 +42,17 @@ module.exports = {
                     keys: [13,15,21]
                 },
                 optionB: {
-                    name: "smash that snooze button!",
+                    name: "Smash that snooze button!",
                     keys: [4,5,11,12,16,20,22,23]
                 },
                 optionC: {
-                    name: "I've had a cup of coffee and am reading the paper",
+                    name: "I've had a cup of coffee and am reading the paper, easing into the day.",
                     keys: [1,2,3,6,7,8,9,10,14,17,18,19,24,25]
                 },
             }
         },
         {
-            question: "Your friend spilled red wine on your carpet, you...",
+            question: "Your friend spilled red wine on your carpet...",
             options: {
                 optionA: {
                     name:  "It's fine, the rug is old and I'll just hide that corner under the couch!",
@@ -72,15 +72,15 @@ module.exports = {
             question: "You meet someone for the first time at a party and your friend thinks they were unfriendly...",
             options: {
                 optionA: {
-                    name: "Totally, such bad vibes",
+                    name: "Totally, such bad vibes!",
                     keys: [1,3,4,5,6,10,12,13,14,16,17,19,20,22,23,24,25]
                 }, 
                 optionB: {
-                    name: "Maybe? We did just meet, some people don't give great first impressions",
+                    name: "Maybe? We did just meet, some people don't give great first impressions.",
                     keys: [2,7,11,18,21]
                 },
                 optionC: {
-                    name: "I am sure they were just shy! I'd love to talk to them more, I am sure there is a reason they got invited!",
+                    name: "They were just shy! I'd love to talk to them more, I am sure there is a reason they got invited!",
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 }
             }
@@ -89,15 +89,15 @@ module.exports = {
             question: "You're headed to the airport, where is your passport?",
             options: {
                 optionA: {
-                    name: "somewhere in my bag, oh whoops it's unzipped!",
+                    name: "Somewhere in my bag, oh whoops it's unzipped!",
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 }, 
                 optionB: {
-                    name: "in my pocket, easily accessible for going through security",
+                    name: "In my pocket, easily accessible for going through security.",
                     keys: [1,2,3,6,8,9,14,15,17,19,23]
                 },
                 optionC: {
-                    name:"in a hidden pouch attached to my body, there are pick-pockets you know!",
+                    name:"In a hidden pouch attached to my body, there are pick-pockets you know!",
                     keys: [2,8,9,15,19]
                 }
             }
@@ -106,24 +106,24 @@ module.exports = {
             question: "You find out your teacher has a favorite student, you...",
             options: {
                 optionA: {
-                    name: "think that is unfair, all students should be treated equally",
+                    name: "Think that is unfair, all students should be treated equally!",
                     keys: [2,6,8,10,11,13,16,18,19,20,21,22,23,24]
                 }, 
                 optionB: {
-                    name: "I guess that is an inevitability, they are human after all, and not all students produce the same work",
+                    name: "Guess that is an inevitability, they are human after all, and not all students produce the same work.",
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 },
                 optionC: {
-                    name: "hope it is you, it'll be SO dissapointing if it isn't you",
+                    name: "Hope it is you, it'll be SO dissapointing if it isn't you!",
                     keys: [2,6,8,10,11,13,16,18,19,20,21,22,23,24]
                 }
             }
         },
         {
-            question: "You have two friends that are in a fight, you: a. make plans with them separately, you hope they work it out but it isn't your job to manage their relationship",
+            question: "You have two friends that are in a fight, you...",
             options: {
                 optionA: {
-                    name: "make plans with them separately, you hope they work it out but it isn't your job to manage their relationship",
+                    name: "Make plans with them separately. Hopefully they work it out but it isn't your job to manage their relationship.",
                     keys: [2,8,9,11,15,19,25]
                 },
                 optionB: {
@@ -131,7 +131,7 @@ module.exports = {
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 }, 
                 optionC: {
-                    name: "Invite the friend you have a stronger relationship with over. You hate drama",
+                    name: "Invite the friend you have a stronger relationship with over. You hate drama!",
                     keys: [8,15,19]
                 }
             }
@@ -140,7 +140,7 @@ module.exports = {
             question: "Your car needs an expensive fix to pass inspection, you...",
             options: {
                 optionA: {
-                    name: "are glad you have an emergency fund just for this type of occassion!",
+                    name: "Are glad you have an emergency fund just for this type of occassion!",
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 }, 
                 optionB: {
@@ -157,11 +157,11 @@ module.exports = {
             question: "You show up to your cousin's wedding and your friend is wearing the exact same jumpsuit, you...",
             options: {
                 optionA: {
-                    name: "love it! Let's get a picture together!",
+                    name: "Love it! Let's get a picture together!",
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 },
                 optionB: {
-                    name: "Turn right around, you packed another outfit just in case",
+                    name: "Turn right around, you packed another outfit just in case.",
                     keys: [4,5,8,9,10,16,18,19,20,22,23,25]
                 }, 
                 optionC: {
@@ -174,7 +174,7 @@ module.exports = {
             question: "You're looking for a house to buy and your realtor suggests a home close to train tracks, you think...",
             options: {
                 optionA: {
-                    name: "Deal breaker, I'm a light sleeper as it is, plus I work from home!",
+                    name: "Deal breaker! I'm a light sleeper as it is, plus I work from home!",
                     keys: [4,5,9,13,16,20,22,23]
                 },
                 optionB: {
@@ -191,28 +191,28 @@ module.exports = {
             question: "Your friend joined a new gym and is trying to get you to join with her, you say...",
             options: {
                 optionA: {
-                    name: "Definitely! It'll be great to workout with a friend",
+                    name: "Definitely! It'll be great to workout with a friend.",
                     keys: [1,2,3,8,10,14,19,21]
                 },
                 optionB: {
-                    name: "No thank you. I don't need to pay for something I won't use",
+                    name: "No thank you. I don't need to pay for something I won't use.",
                     keys: [4,5,7,11,12,16,17,18,20,22,23,24]
                 }, 
                 optionC: {
-                    name: "Maybe. You already belong to a gym and frequent several studios to diverisfy my weekly workout regimine, but what do they offer?",
+                    name: "Maybe? You already belong to a gym and frequent several studios, but what do they offer?",
                     keys: [6,9,13,15,25]
                 }
             }
         },
         {
-            question: "Your idea of grocery shopping with your partner looks like:",
+            question: "Your idea of grocery shopping with your partner looks like...",
             options: {
                 optionA: {
                     name: "Divide and conquer, let's make this errand go by quickly!",
                     keys: [7,8,13,15,19,21]
                 },
                 optionB: {
-                    name: "Slowly go through the aisles and look for inspiration, hey look at how good these apples look!",
+                    name: "Slowly go through the aisles and look for inspiration. Hey look at how good these apples look!",
                     keys: [1,2,3,4,5,6,9,10,11,12,14,16,17,18,20,22,23,24,25]
                 }, 
                 optionC: {
@@ -222,18 +222,18 @@ module.exports = {
             }
         },
         {
-            question: "Your nephew is at an inquisitive age and has locked his attention and questions in on you. Your response:",
+            question: "Your nephew is at an inquisitive age and has locked his attention and questions in on you. Your response...",
             options: {
                 optionA: {
-                    name: "Hey buddy, where are your parents? You're not interested in entertaining him",
+                    name: "Hey buddy, where are your parents? You're not interested in entertaining him!",
                     keys: [4,5,7]
                 },
                 optionB: {
-                    name: "Thoughtfully answering all of his questions and marvelling to yourself about how interesting his thought process is at that age",
+                    name: "Thoughtfully answer all of his questions and marvel to yourself about how interesting his thought process is at that age",
                     keys: [1,2,3,6,8,9,13,19,25]
                 }, 
                 optionC: {
-                    name: "Hang out for a bit with him and have fun but eventually distract him with a new activity and make your escape",
+                    name: "Hang out for a bit with him and have fun but eventually distract him with a new activity to make your escape",
                     keys: [10,11,12,14,15,16,17,18,20,21,22,23,24]
                 }
             }
@@ -242,7 +242,7 @@ module.exports = {
             question: "You got an amazing job offer in another city, which means leaving your best friend and wonderful apartment behind, you...",
             options: {
                 optionA: {
-                    name: "Accept the offer, you'll miss your friend and the life you built but there are new relationships and life experiencing awaiting you, that's life",
+                    name: "Accept the offer, you'll miss your friend and the life you built but there are new relationships and life experiencing awaiting you, that's life!",
                     keys: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 },
                 optionB: {
