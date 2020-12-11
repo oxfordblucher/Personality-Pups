78f71645965d19e3e20b9a832f44b2a55207cc86
oxfordblucher <hiuchanhk@gmail.com>
2020-12-10 19:14:17 -0500

    working dog api calls

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,44 +1,67 @@
 const db = require("../models/index");
 const path = require("path");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-const questions = require("../questions");
-const dogPics = require("../dogpics");
+const questions = require("../questions")
+const axios = require("axios");
 
-module.exports = function(app) {
-    
-    app.get("/", function(req, res) {
-        res.render("landingpage");
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+        axios.get('https://dog.ceo/api/breeds/image/random').then(function (data) {
+
+            const picUrlObj = {
+                picUrl: data.data.message
+            };
+
+            res.render('landingpage', picUrlObj);
+        })
     });
+   
 
-    app.get("/question/:number", function(req, res) {
-        const qObj = {
-            id: req.params.number,
-            question: questions.questions[req.params.number].question,
-            optionA: {
-                qID: req.params.number,
-                name: questions.questions[req.params.number].options.optionA.name,
-                keys: questions.questions[req.params.number].options.optionA.keys
-            },
-            optionB: {
-                qID: req.params.number,
-                name: questions.questions[req.params.number].options.optionB.name,
-                keys: questions.questions[req.params.number].options.optionB.keys
-            },
-            optionC: {
-                qID: req.params.number,
-                name: questions.questions[req.params.number].options.optionC.name,
-                keys: questions.questions[req.params.number].options.optionC.keys
+    app.get("/question/:number", function (req, res) {
+        axios.get('https://dog.ceo/api/breeds/image/random').then(function (data) {
+
+            let picUrl = data.data.message;
+            
+            const qObj = {
+                picUrl: picUrl,
+                id: req.params.number,
+                question: questions.questions[req.params.number].question,
+                optionA: {
+                    qID: req.params.number,
+                    name: questions.questions[req.params.number].options.optionA.name,
+                    keys: questions.questions[req.params.number].options.optionA.keys
+                },
+                optionB: {
+                    qID: req.params.number,
+                    name: questions.questions[req.params.number].options.optionB.name,
+                    keys: questions.questions[req.params.number].options.optionB.keys
+                },
+                optionC: {
+                    qID: req.params.number,
+                    name: questions.questions[req.params.number].options.optionC.name,
+                    keys: questions.questions[req.params.number].options.optionC.keys
+                }
             }
-        }
-        res.render("question-block", qObj);
+            res.render("question-block", qObj);
+        })
+
     });
 
-    app.get("/result", function(req, res) {
+    app.get("/result", function (req, res) {
         res.render("results");
     });
 
-    app.get("/login", function(req, res) {
+    app.get("/login", function (req, res) {
         res.render("login");
     });
 
 };
+
+function getPic() {
+    axios.get('https://dog.ceo/api/breeds/image/random').then(function (data) {
+        console.log(data);
+        let picUrl = data.message;
+        return picUrl;
+    })
+}
